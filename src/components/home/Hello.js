32b1a237dd4a487cb5dd786154abbf2bc6d7ab0b
Hisham Haniffa <hishamhaniffa4@gmail.com>
2017-03-03 01:01:20 +0530

    Code refactor and added component based Arch

---
+++ src/components/home/Hello.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export class Hello extends Component {
+    render() {
+		return (
+			<div className="placeholder">
+				<p>This is a react component</p>
+				<p>Component based architecture</p>
+			</div>
+        );
+    }
+}
